24592a4cd326994a05d4732280a474f1e48c1f91
NGUYEN11235 <66746409+NGUYEN11235@users.noreply.github.com>
2023-07-06 15:06:15 +0700

    Initial commit

---
+++ GUI_label.py
@@ -0,0 +1,467 @@
+from PyQt5 import QtWidgets, uic
+from config import get_config
+from canvas import Canvas
+from shape import Shape
+from PyQt5 import QtGui
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QFileDialog
+from label_dialog import LabelDialog
+from label_list_widget import LabelListWidget, LabelListWidgetItem
+from unique_label_qlist_widget import UniqueLabelQListWidget
+from PyQt5.QtGui import QPixmap
+from zoom_widget import ZoomWidget
+import math
+import html
+import os.path as osp
+import imgviz
+
+
+LABEL_COLORMAP = imgviz.label_colormap()
+
+
+
+class MyWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        uic.loadUi('GUI_label.ui', self)
+        config = get_config()
+        self._config = config
+        Shape.line_color = QtGui.QColor(*self._config["shape"]["line_color"])
+        Shape.fill_color = QtGui.QColor(*self._config["shape"]["fill_color"])
+        Shape.select_line_color = QtGui.QColor(
+            *self._config["shape"]["select_line_color"]
+        )
+        Shape.select_fill_color = QtGui.QColor(
+            *self._config["shape"]["select_fill_color"]
+        )
+        Shape.vertex_fill_color = QtGui.QColor(
+            *self._config["shape"]["vertex_fill_color"]
+        )
+        Shape.hvertex_fill_color = QtGui.QColor(
+            *self._config["shape"]["hvertex_fill_color"]
+        )
+
+        # Set point size from config file
+        Shape.point_size = self._config["shape"]["point_size"]
+
+
+        self.uniqLabelList = UniqueLabelQListWidget()
+        self.uniqLabelList.setToolTip(
+            self.tr(
+                "Select label to start annotating for it. "
+                "Press 'Esc' to deselect."
+            )
+        )
+
+
+        self.labelDialog = LabelDialog(
+            parent=self,
+            labels=self._config["labels"],
+            sort_labels=self._config["sort_labels"],
+            show_text_field=self._config["show_label_text_field"],
+            completion=self._config["label_completion"],
+            fit_to_content=self._config["fit_to_content"],
+            flags=self._config["label_flags"],
+        )
+
+        self.labelList = LabelListWidget()
+        self.lastOpenDir = None
+
+        # self.flag_dock = self.flag_widget = None
+        # self.flag_dock = QtWidgets.QDockWidget(self.tr("Flags"), self)
+        # self.flag_dock.setObjectName("Flags")
+        self.flag_widget = QtWidgets.QListWidget()
+        if config["flags"]:
+            self.loadFlags({k: False for k in config["flags"]})
+        self.gridLayout_2.addWidget(self.flag_widget)
+        self.flag_widget.itemChanged.connect(self.setDirty)
+
+        self.labelList.itemSelectionChanged.connect(self.labelSelectionChanged)
+        self.labelList.itemDoubleClicked.connect(self.editLabel)
+        self.labelList.itemChanged.connect(self.labelItemChanged)
+        self.labelList.itemDropped.connect(self.labelOrderChanged)
+        # self.shape_dock = QtWidgets.QDockWidget(
+        #     self.tr("Polygon Labels"), self
+        # )
+        self.gridLayout_3.addWidget(self.labelList)
+
+
+        self.uniqLabelList = UniqueLabelQListWidget()
+        self.uniqLabelList.setToolTip(
+            self.tr(
+                "Select label to start annotating for it. "
+                "Press 'Esc' to deselect."
+            )
+        )
+
+        if self._config["labels"]:
+            for label in self._config["labels"]:
+                item = self.uniqLabelList.createItemFromLabel(label)
+                self.uniqLabelList.addItem(item)
+                rgb = self._get_rgb_by_label(label)
+                self.uniqLabelList.setItemLabel(item, label, rgb)
+        # self.label_dock = QtWidgets.QDockWidget(self.tr("Label List"), self)
+        # self.label_dock.setObjectName("Label List")
+        self.gridLayout_4.addWidget(self.uniqLabelList)
+
+
+        self.fileSearch = QtWidgets.QLineEdit()
+        self.fileSearch.setPlaceholderText(self.tr("Search Filename"))
+        self.fileSearch.textChanged.connect(self.fileSearchChanged)
+        self.fileListWidget = QtWidgets.QListWidget()
+        self.fileListWidget.itemSelectionChanged.connect(self.fileSelectionChanged)
+        fileListLayout = QtWidgets.QVBoxLayout()
+        fileListLayout.setContentsMargins(0, 0, 0, 0)
+        fileListLayout.setSpacing(0)
+        fileListLayout.addWidget(self.fileSearch)
+        fileListLayout.addWidget(self.fileListWidget)
+        # self.file_dock = QtWidgets.QDockWidget(self.tr("File List"), self)
+        # self.file_dock.setObjectName("Files")
+        fileListWidget = QtWidgets.QWidget()
+        fileListWidget.setLayout(fileListLayout)
+        self.gridLayout_5.addWidget(fileListWidget)
+
+        self.canvas = self.labelList.canvas = Canvas(
+            epsilon=self._config["epsilon"],
+            double_click=self._config["canvas"]["double_click"],
+            num_backups=self._config["canvas"]["num_backups"],
+            crosshair=self._config["canvas"]["crosshair"],
+        )
+
+        self.zoomWidget = ZoomWidget()
+        zoom = QtWidgets.QWidgetAction(self)
+        # zoomBoxLayout = QtWidgets.QVBoxLayout()
+        # zoomBoxLayout.addWidget(self.zoomWidget)
+        zoomLabel = QtWidgets.QLabel("Zoom")
+        zoomLabel.setAlignment(Qt.AlignCenter)
+        zoomLabel.setFont(QtGui.QFont(None, 10))
+        zoom.setDefaultWidget(QtWidgets.QWidget())
+        self.zoomWidget.setEnabled(False)
+        self.zoom_action = QtWidgets.QAction(QtGui.QIcon('icons/zoom-in.png'), 'Zoom In', self)
+        self.zoom_action.setShortcut(QtGui.QKeySequence("Ctrl+Wheel"))
+        self.zoom_action.triggered.connect(self.addZoom)
+
+        self.canvas.zoomRequest.connect(self.zoomRequest)
+        self.canvas.newShape.connect(self.newShape)
+        self.canvas.shapeMoved.connect(self.setDirty)
+        self.canvas.selectionChanged.connect(self.shapeSelectionChanged)
+        # self.canvas.drawingPolygon.connect(self.toggleDrawingSensitive)
+        self.canvas.scrollRequest.connect(self.scrollRequest)
+
+        scrollArea = QtWidgets.QScrollArea()
+        scrollArea.setWidget(self.canvas)
+        scrollArea.setWidgetResizable(True)
+        self.scrollBars = {
+            Qt.Vertical: scrollArea.verticalScrollBar(),
+            Qt.Horizontal: scrollArea.horizontalScrollBar(),
+        }
+
+        self.gridLayout.addWidget(scrollArea)
+        self.open_file_btn.clicked.connect(self.load_file)
+        self.polygon_btn.clicked.connect(self.create_polygon)
+        self.edit_btn.clicked.connect(self.edit_mode)
+
+
+    def load_file(self):
+        file_dialog = QFileDialog()
+        file_path, _ = file_dialog.getOpenFileName(self, "Select Image File", "", "Image Files (*.png *.jpg *.jpeg)")
+        if file_path:
+            self.current_image_path = file_path
+            image = QPixmap(file_path)
+            self.canvas.loadPixmap(image)
+
+
+    def create_polygon(self):
+        self.canvas.setEditing(False)
+        self.canvas.createMode = 'polygon'
+
+    def edit_mode(self):
+        self.canvas.setEditing(True)
+
+
+    def labelItemChanged(self, item):
+        shape = item.shape()
+        self.canvas.setShapeVisible(shape, item.checkState() == Qt.Checked)
+
+
+    def labelSelectionChanged(self):
+        if self.canvas.editing():
+            selected_shapes = []
+            for item in self.labelList.selectedItems():
+                selected_shapes.append(item.shape())
+            if selected_shapes:
+                self.canvas.selectShapes(selected_shapes)
+            else:
+                self.canvas.deSelectShape()
+
+
+    def labelOrderChanged(self):
+        self.setDirty()
+        self.canvas.loadShapes([item.shape() for item in self.labelList])
+
+
+    def editLabel(self, item=None):
+        if item and not isinstance(item, LabelListWidgetItem):
+            raise TypeError("item must be LabelListWidgetItem type")
+
+        if not self.canvas.editing():
+            return
+        if not item:
+            item = self.currentItem()
+        if item is None:
+            return
+        shape = item.shape()
+        if shape is None:
+            return
+        text, flags, group_id, description = self.labelDialog.popUp(
+                text=shape.label,
+            flags=shape.flags,
+            group_id=shape.group_id,
+            description=shape.description,
+        )
+        if text is None:
+            return
+
+        shape.label = text
+        shape.flags = flags
+        shape.group_id = group_id
+        shape.description = description
+
+        self._update_shape_color(shape)
+        if shape.group_id is None:
+            item.setText(
+                '{} <font color="#{:02x}{:02x}{:02x}">●</font>'.format(
+                    html.escape(shape.label), *shape.fill_color.getRgb()[:3]
+                )
+            )
+        else:
+            item.setText("{} ({})".format(shape.label, shape.group_id))
+        self.setDirty()
+        if self.uniqLabelList.findItemByLabel(shape.label) is None:
+            item = self.uniqLabelList.createItemFromLabel(shape.label)
+            self.uniqLabelList.addItem(item)
+            rgb = self._get_rgb_by_label(shape.label)
+            self.uniqLabelList.setItemLabel(item, shape.label, rgb)
+
+    def loadFlags(self, flags):
+        self.flag_widget.clear()
+        for key, flag in flags.items():
+            item = QtWidgets.QListWidgetItem(key)
+            item.setFlags(item.flags() | Qt.ItemIsUserCheckable)
+            item.setCheckState(Qt.Checked if flag else Qt.Unchecked)
+            self.flag_widget.addItem(item)
+
+
+    def _update_shape_color(self, shape):
+        r, g, b = self._get_rgb_by_label(shape.label)
+        shape.line_color = QtGui.QColor(r, g, b)
+        shape.vertex_fill_color = QtGui.QColor(r, g, b)
+        shape.hvertex_fill_color = QtGui.QColor(255, 255, 255)
+        shape.fill_color = QtGui.QColor(r, g, b, 128)
+        shape.select_line_color = QtGui.QColor(255, 255, 255)
+        shape.select_fill_color = QtGui.QColor(r, g, b, 155)
+
+    def _get_rgb_by_label(self, label):
+        if self._config["shape_color"] == "auto":
+            item = self.uniqLabelList.findItemByLabel(label)
+            if item is None:
+                item = self.uniqLabelList.createItemFromLabel(label)
+                self.uniqLabelList.addItem(item)
+                rgb = self._get_rgb_by_label(label)
+                self.uniqLabelList.setItemLabel(item, label, rgb)
+            label_id = self.uniqLabelList.indexFromItem(item).row() + 1
+            label_id += self._config["shift_auto_shape_color"]
+            return LABEL_COLORMAP[label_id % len(LABEL_COLORMAP)]
+        elif (
+            self._config["shape_color"] == "manual"
+            and self._config["label_colors"]
+            and label in self._config["label_colors"]
+        ):
+            return self._config["label_colors"][label]
+        elif self._config["default_shape_color"]:
+            return self._config["default_shape_color"]
+        return (0, 255, 0)
+
+
+    def setDirty(self):
+        # Even if we autosave the file, we keep the ability to undo
+        # self.actions.undo.setEnabled(self.canvas.isShapeRestorable)
+
+        if self._config["auto_save"]:
+            label_file = osp.splitext(self.imagePath)[0] + ".json"
+            # if self.output_dir:
+            #     label_file_without_path = osp.basename(label_file)
+            #     label_file = osp.join(self.output_dir, label_file_without_path)
+            # self.saveLabels(label_file)
+            # return
+        self.dirty = True
+        # self.actions.save.setEnabled(True)
+        title = 'label_tool'
+
+        self.setWindowTitle(title)
+
+
+    def currentItem(self):
+        items = self.labelList.selectedItems()
+        if items:
+            return items[0]
+        return None
+
+
+    def fileSearchChanged(self):
+        self.importDirImages(
+            self.lastOpenDir,
+            pattern=self.fileSearch.text(),
+            load=False,
+        )
+
+    def fileSelectionChanged(self):
+        items = self.fileListWidget.selectedItems()
+        if not items:
+            return
+        item = items[0]
+
+        currIndex = self.imageList.index(str(item.text()))
+        if currIndex < len(self.imageList):
+            filename = self.imageList[currIndex]
+            if filename:
+                self.loadFile(filename)
+
+    def scrollRequest(self, delta, orientation):
+        units = -delta * 0.1  # natural scroll
+        bar = self.scrollBars[orientation]
+        value = bar.value() + bar.singleStep() * units
+        self.setScroll(orientation, value)
+
+    def setScroll(self, orientation, value):
+        self.scrollBars[orientation].setValue(int(value))
+        # self.scroll_values[orientation][self.filename] = value
+
+    def setZoom(self, value):
+        # self.actions.fitWidth.setChecked(False)
+        # self.actions.fitWindow.setChecked(False)
+        self.zoomWidget.setValue(value)
+        self.paintCanvas()
+        # self.zoom_values[self.filename] = (self.zoomMode, value)
+
+    def addZoom(self, increment=1.1):
+        zoom_value = self.zoomWidget.value() * increment
+        print(zoom_value)
+        if increment > 1:
+            zoom_value = math.ceil(zoom_value)
+        else:
+            zoom_value = math.floor(zoom_value)
+        self.setZoom(zoom_value)
+
+
+    def zoomRequest(self, delta, pos):
+        canvas_width_old = self.canvas.width()
+        units = 1.1
+        if delta < 0:
+            units = 0.9
+        self.addZoom(units)
+
+        canvas_width_new = self.canvas.width()
+        if canvas_width_old != canvas_width_new:
+            canvas_scale_factor = canvas_width_new / canvas_width_old
+
+            x_shift = round(pos.x() * canvas_scale_factor) - pos.x()
+            y_shift = round(pos.y() * canvas_scale_factor) - pos.y()
+
+            self.setScroll(
+                Qt.Horizontal,
+                self.scrollBars[Qt.Horizontal].value() + x_shift,
+            )
+            self.setScroll(
+                Qt.Vertical,
+                self.scrollBars[Qt.Vertical].value() + y_shift,
+            )
+
+    def paintCanvas(self):
+        # assert not self.image.isNull(), "cannot paint null image"
+        self.canvas.scale = 0.01 * self.zoomWidget.value()
+        self.canvas.adjustSize()
+        self.canvas.update()
+
+    def newShape(self):
+        """Pop-up and give focus to the label editor.
+
+        position MUST be in global coordinates.
+        """
+        items = self.uniqLabelList.selectedItems()
+        text = None
+        if items:
+            text = items[0].data(Qt.UserRole)
+        flags = {}
+        group_id = None
+        description = ""
+        if self._config["display_label_popup"] or not text:
+            previous_text = self.labelDialog.edit.text()
+            text, flags, group_id, description = self.labelDialog.popUp(text)
+            if not text:
+                self.labelDialog.edit.setText(previous_text)
+
+        # if text and not self.validateLabel(text):
+        #     self.errorMessage(
+        #         self.tr("Invalid label"),
+        #         self.tr("Invalid label '{}' with validation type '{}'").format(
+        #             text, self._config["validate_label"]
+        #         ),
+        #     )
+        #     text = ""
+        if text:
+            self.labelList.clearSelection()
+            shape = self.canvas.setLastLabel(text, flags)
+            shape.group_id = group_id
+            shape.description = description
+            self.addLabel(shape)
+            # self.actions.editMode.setEnabled(True)
+            # self.actions.undoLastPoint.setEnabled(False)
+            # self.actions.undo.setEnabled(True)
+            self.setDirty()
+        else:
+            self.canvas.undoLastLine()
+            self.canvas.shapesBackups.pop()
+
+    def shapeSelectionChanged(self, selected_shapes):
+        self._noSelectionSlot = True
+        for shape in self.canvas.selectedShapes:
+            shape.selected = False
+        self.labelList.clearSelection()
+        self.canvas.selectedShapes = selected_shapes
+        for shape in self.canvas.selectedShapes:
+            shape.selected = True
+            item = self.labelList.findItemByShape(shape)
+            self.labelList.selectItem(item)
+            self.labelList.scrollToItem(item)
+        self._noSelectionSlot = False
+        n_selected = len(selected_shapes)
+
+    def addLabel(self, shape):
+        if shape.group_id is None:
+            text = shape.label
+        else:
+            text = "{} ({})".format(shape.label, shape.group_id)
+        label_list_item = LabelListWidgetItem(text, shape)
+        self.labelList.addItem(label_list_item)
+        if self.uniqLabelList.findItemByLabel(shape.label) is None:
+            item = self.uniqLabelList.createItemFromLabel(shape.label)
+            self.uniqLabelList.addItem(item)
+            rgb = self._get_rgb_by_label(shape.label)
+            self.uniqLabelList.setItemLabel(item, shape.label, rgb)
+        self.labelDialog.addLabelHistory(shape.label)
+        # for action in self.actions.onShapesPresent:
+        #     action.setEnabled(True)
+
+        self._update_shape_color(shape)
+        label_list_item.setText(
+            '{} <font color="#{:02x}{:02x}{:02x}">●</font>'.format(
+                html.escape(text), *shape.fill_color.getRgb()[:3]
+            )
+        )
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    window = MyWindow()
+    window.show()
+    app.exec_()
